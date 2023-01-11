f460b83f6599031c50accd25f14bea4d58af2d30
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2023-01-10 22:20:47 -0300

    fix: remove root path on register serviceWorker.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", showSushis);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("serviceWorker.js")
       .then(() => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
