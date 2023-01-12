3c107432cef2e7033e5ef4a4fd1fe46fb1098439
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2023-01-12 14:41:10 -0300

    fix: relative paths to cache assets

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,12 @@
 const staticDevCoffee = "dev-oche-sushi-v1";
 const assets = [
-  "",
-  "index.html",
+  "./",
   "css/style.css",
   "js/app.js",
   "images/hot.jpg",
   "images/niguiri.jpg",
   "images/skin.jpg",
-  "images/uramki.webp"
+  "images/uramaki.webp"
 ];
 
 self.addEventListener("install", installEvent => {
